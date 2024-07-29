fbb87ca8ace7ee567fddb129651a62373c16efd9
Rishabh Shukla <rishabhshukla1333@gmail.com>
2024-07-30 02:33:35 +0530

    get and delete method of income done

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,4 +1,6 @@
-const IncomeSchema=require('../models/incomeModel')
+const IncomeSchema=require('../models/incomeModel');
+
+//to add income
 exports.addIncome= async (req,res)=>{
     const {title,amount,type,category,description,date}=req.body;
 
@@ -23,8 +25,31 @@ exports.addIncome= async (req,res)=>{
         await income.save();
         res.status(200).json({message: "Data saved successfully"})
     } catch (error) {
-        
+        res.status(404).json({message: "Unsuccessful"})
     }
 
-    // console.log(income);
+}
+
+//to fetch income
+exports.getIncome=async (req,res)=>{
+    try {
+        const incomes= await IncomeSchema.find().sort({createdAt:-1});
+        res.status(200).json(incomes);
+    } catch (error) {
+        res.status(500).json({message: "Server Error"});
+    }
+}
+
+//to delete income
+exports.deleteIncome=async (req,res)=>{
+   const {id}=req.params;
+   IncomeSchema.findByIdAndDelete(id)
+   .then((income)=>{
+    res.status(200).json({message:" Income deleted Successfully"})
+   })
+   .catch((error)=>{
+    res.status(500).json({message:" Failed to delete income"+error})
+   })
+
+
 }
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,9 @@
-const { addIncome } = require('../controllers/income');
+const { addIncome, getIncome, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router();
 
-router.post('/add-income',addIncome)
+router.post('/add-income',addIncome);
+router.get('/get-income',getIncome);
+router.delete('/delete-income/:id',deleteIncome)
 
 module.exports=router
